ff050bda3869239d4c14c79354486beff2d56eaf
Erick 光 <erickcestari03@gmail.com>
2024-12-09 15:59:19 -0300

    refactor day 6 part 1

--- src/day_6/part_1.rs
+++ src/day_6/part_1.rs
@@ -19,52 +19,35 @@ pub fn run() -> String {
     }
 
     loop {
-        let (row, col, dir) = guard;
+        let (row, col, _) = guard;
+        visited.insert((row, col));
 
-        match dir {
-            '^' => {
-                if row == 0 || matrix[row - 1][col] == '#' {
-                    guard.2 = '>';
-                } else {
-                    guard = (row - 1, col, dir);
-                    visited.insert((row - 1, col));
-                }
-            }
-            '>' => {
-                if col == matrix[row].len() - 1 || matrix[row][col + 1] == '#' {
-                    guard.2 = 'v';
-                } else {
-                    guard = (row, col + 1, dir);
-                    visited.insert((row, col + 1));
-                }
-            }
-            'v' => {
-                if row == matrix.len() - 1 || matrix[row + 1][col] == '#' {
-                    guard.2 = '<';
-                } else {
-                    guard = (row + 1, col, dir);
-                    visited.insert((row + 1, col));
-                }
-            }
-            '<' => {
-                if col == 0 || matrix[row][col - 1] == '#' {
-                    guard.2 = '^';
-                } else {
-                    guard = (row, col - 1, dir);
-                    visited.insert((row, col - 1));
-                }
-            }
-            _ => unreachable!(),
-        }
+        guard = next_guard_position(guard, &matrix);
 
         if guard.0 >= matrix.len() - 1
             || guard.1 >= matrix[guard.0].len() - 1
             || guard.0 == 0
             || guard.1 == 0
         {
+            visited.insert((guard.0, guard.1));
             break;
         }
     }
 
     visited.len().to_string()
+}
+
+fn next_guard_position(guard: (usize, usize, char), matrix: &[Vec<char>]) -> (usize, usize, char) {
+    let (row, col, dir) = guard;
+    match dir {
+        '^' if matrix[row - 1][col] != '#' => (row - 1, col, '^'),
+        '>' if matrix[row][col + 1] != '#' => (row, col + 1, '>'),
+        'v' if matrix[row + 1][col] != '#' => (row + 1, col, 'v'),
+        '<' if matrix[row][col - 1] != '#' => (row, col - 1, '<'),
+        '^' => (row, col, '>'),
+        '>' => (row, col, 'v'),
+        'v' => (row, col, '<'),
+        '<' => (row, col, '^'),
+        _ => unreachable!(),
+    }
 }
