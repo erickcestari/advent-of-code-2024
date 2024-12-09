baee212cbf91fca58c8ee0c818b84cf7ba7c421f
Erick 光 <erickcestari03@gmail.com>
2024-12-09 15:59:05 -0300

    add benches day 6

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -13,6 +13,8 @@ fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("d4-p1", |b| b.iter(day_4::part_2::run));
     c.bench_function("d5-p1", |b| b.iter(day_5::part_1::run));
     c.bench_function("d5-p2", |b| b.iter(day_5::part_2::run));
+    c.bench_function("d6-p1", |b| b.iter(day_6::part_1::run));
+    c.bench_function("d6-p2", |b| b.iter(day_6::part_2::run));
 }
 
 criterion_group!(benches, criterion_benchmark);
